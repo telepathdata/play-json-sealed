b4c030b77f5f6a4c5da0000ea2af0743c80f0fcf
Sciss <contact@sciss.de>
2013-06-17 21:21:35 +0200

    passes

--- core/src/main/scala/play/api/libs/json/SealedTraitFormat.scala
+++ core/src/main/scala/play/api/libs/json/SealedTraitFormat.scala
@@ -4,7 +4,7 @@ import scala.reflect.macros.Context
 import language.experimental.macros
 
 object SealedTraitFormat {
-  private val DEBUG   = true
+  private val DEBUG   = false
   private val PACKAGE = false
 
   def apply[A]: Format[A] = macro applyImpl[A]
@@ -26,35 +26,40 @@ object SealedTraitFormat {
       log("")
     }
     import c.universe._
-    val aIdent        = Ident(aClazz)
+    val aIdent  = Ident(aClazz)
 
-    val cases = subs.toList.map { sub =>
-      val subIdent  = Ident(sub.asClass)
-      // val subIdentExpr = c.Expr[A](subIdent)
+    val subsAll = subs.toList.sortBy(_.name.toString)
+    val cases   = subsAll.map { sub =>
+      val subC      = sub.asClass
+      val isObject  = subC.isModuleClass
+      val subIdent  = Ident(if (isObject) subC.companionSymbol.asModule else subC)
       val patWrite  = Bind(newTermName("x"), Typed(Ident("_"), subIdent))
       val subName0  = if (PACKAGE) sub.fullName else sub.name.toString
       val subName   = Literal(Constant(subName0))
       val patRead   = subName
-      val (bodyWrite, bodyRead) = if (sub.isModuleClass) {
+      val (bodyWrite, bodyRead) = if (isObject) {
         val jsSuccessTree = Ident(typeOf[JsSuccess.type].typeSymbol.asClass.companionSymbol.asModule) // frickin' hell
-        val subIdent2     = Ident(sub.asClass.companionSymbol.asModule)
         Apply(Ident("writeObject"), subName :: Nil) ->
-        Apply(TypeApply(jsSuccessTree, aIdent :: Nil), subIdent2 :: Nil) // JsSuccess[A](MyCaseObject)
+        Apply(TypeApply(jsSuccessTree, aIdent :: Nil), subIdent :: Nil) // JsSuccess[A](MyCaseObject)
 
       } else {
         val jsonTree  = Ident(typeOf[Json.type].typeSymbol.asClass)
         val subFmt    = TypeApply(Select(jsonTree, "format"), subIdent :: Nil)
-        Apply(TypeApply(Ident("writeClass"), subIdent:: Nil), subName :: Ident("x") :: subFmt :: Nil) ->
-        (reify { ??? } .tree)
+        Apply(TypeApply(Ident("writeClass"), subIdent:: Nil), subName :: Ident("x"   ) :: subFmt :: Nil) ->
+        Apply(Select(subFmt, "reads"), Ident("data") :: Nil)
       }
       CaseDef(patWrite, bodyWrite) ->
-      CaseDef(patRead , bodyRead )
+      (isObject, CaseDef(patRead, bodyRead))
     }
     val (casesWrite, casesRead) = cases.unzip
     val matchWrite      = Match(Ident("value"), casesWrite)
     val matchWriteExpr  = c.Expr[JsValue](matchWrite)
-    val matchRead       = Match(Ident("name"), casesRead)
-    val matchReadExpr   = c.Expr[JsResult[A]](matchRead)
+    val casesReadC      = casesRead collect { case (false, tree) => tree }
+    val matchReadC      = Match(Ident("name"), casesReadC) // XXX TODO add catch all
+    val matchReadExprC  = c.Expr[JsResult[A]](matchReadC)
+    val casesReadO      = casesRead collect { case (true, tree) => tree }
+    val matchReadO      = Match(Ident("name"), casesReadO) // XXX TODO add catch all
+    val matchReadExprO  = c.Expr[JsResult[A]](matchReadO)
     val r               = reify {
       new Format[A] {
         private def writeClass[A1](name: String, obj: A1, w: Writes[A1]): JsValue =
@@ -64,16 +69,22 @@ object SealedTraitFormat {
           JsObject(Seq("class" -> JsString(name)))
 
         def writes(value: A): JsValue = matchWriteExpr.splice
-        def reads(json: JsValue): JsResult[A] = json match {
-          // this crashes upon macro expansion (probably a bug)
-          // case JsObject(Seq(("class", JsString(name)), rest @ _*)) => ...
-          case JsObject(sq) =>
-            sq.toList match {
-              case ("class", JsString(name)) :: ("data", data) :: Nil => ???
-              case ("class", JsString(name)) :: Nil => matchReadExpr.splice
-              case _ => JsError(s"Unexpected JSON dictionary: $json")
-            }
-          case _     => JsError(s"Not a JSON dictionary: $json")
+
+        def reads(json: JsValue): JsResult[A] = try {
+          (json: @unchecked) match {
+            // this crashes upon macro expansion (probably a bug)
+            // case JsObject(Seq(("class", JsString(name)), rest @ _*)) => ...
+            case JsObject(sq) =>
+              (sq.toList: @unchecked) match {
+                case ("class", JsString(name)) :: tail =>
+                  (tail: @unchecked) match {
+                    case ("data", data) :: Nil  => matchReadExprC.splice
+                    case Nil                    => matchReadExprO.splice
+                  }
+              }
+          }
+        } catch {
+          case _: MatchError => JsError(json.toString)
         }
       }
     }
--- test/src/test/scala/play/api/libs/json/FooTest.scala
+++ test/src/test/scala/play/api/libs/json/FooTest.scala
@@ -13,12 +13,17 @@ object FooTest extends App {
   // implicit val fooFmt = Json.format[Foo]
 
   // implicit def fooFmt: Format[Foo] = this
-  val out = Json.toJson(Bar(33))
-  println(Json.prettyPrint(out))
-
-  val out1 = Json.toJson(Gaga)
+  val obj1  = Bar(33)
+  val out1  = Json.toJson(obj1)
   println(Json.prettyPrint(out1))
 
-//  Json.format[Gaga.type]
-  // Json.format[Raga]
+  val in1 = Json.fromJson[Foo](out1)
+  assert(in1.asOpt == Some(obj1), s"result $in1")
+
+  val obj2  = Gaga
+  val out2  = Json.toJson(obj2)
+  println(Json.prettyPrint(out2))
+
+  val in2 = Json.fromJson[Foo](out2)
+  assert(in2.asOpt == Some(obj2), s"result $in2")
 }
