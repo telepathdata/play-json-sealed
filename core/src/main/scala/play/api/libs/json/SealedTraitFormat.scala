ab4963c35f1da79d865fb97502f7191056514b7b
Sciss <contact@sciss.de>
2013-06-17 18:44:55 +0200

    writer almost done

--- core/src/main/scala/play/api/libs/json/SealedTraitFormat.scala
+++ core/src/main/scala/play/api/libs/json/SealedTraitFormat.scala
@@ -3,23 +3,9 @@ package play.api.libs.json
 import scala.reflect.macros.Context
 import language.experimental.macros
 
-//object SealedTraitFormat {
-//  def writesImpl[A: c.TypeTag](c: Context)(expr: c.Expr[A]): c.Expr[JsValue] = {
-//
-//
-//    ???
-////    val (prod: Product, sub) = foo match {
-////      case b: Bar => (b, Json.toJson(b)(barFmt))
-////      case b: Baz => (b, Json.toJson(b)(bazFmt))
-////    }
-////    JsObject(Seq("class" -> JsString(prod.productPrefix), "data" -> sub))
-//  }
-//}
-//trait SealedTraitFormat[A] {
-//  def writes(value: A): JsValue = macro SealedTraitFormat.writesImpl[A]
-//}
-
 object SealedTraitFormat {
+  val DEBUG = false
+
   def apply[A]: Format[A] = macro applyImpl[A]
 
   def applyImpl[A: c.WeakTypeTag](c: Context): c.Expr[Format[A]] = {
@@ -37,40 +23,43 @@ object SealedTraitFormat {
       val subIdent  = Ident(sub.asClass)
       val pat       = Bind(newTermName("x"), Typed(Ident("_"), subIdent))
       // c.inferImplicitValue()
-      val jsonTree  = Ident(typeOf[Json.type].typeSymbol.asClass)
-      val subFmt    = TypeApply(Select(jsonTree, "format"), subIdent :: Nil)
+      val subName   = Literal(Constant(sub.fullName))
+      val body      = if (sub.isModuleClass) {
+        // val jsStringTree = Ident(typeOf[JsString.type].typeSymbol.asClass)
+        // Apply(Select(jsStringTree, "apply"), subName :: Nil)
+        // val subNameExpr = c.Expr[String](subName)
+        // reify { JsObject(Seq("class" -> JsString(subNameExpr.splice))) } .tree
+        Apply(Ident("writeObject"), subName :: Nil)
+
+      } else {
+        //        val subType     = sub.companionSymbol.typeSignature
+        //        val subUnapplyM = subType.declaration(stringToTermName("unapply")).asMethod
+        //        // Json.format does not handle case classes which have no parameters!
+        //        // Therefore, check this out here.
+        //        val unapplyReturnTypes = subUnapplyM.returnType match {
+        //          case TypeRef(_, _, Nil) =>
+        //          case _ =>
+        //
+        val jsonTree  = Ident(typeOf[Json.type].typeSymbol.asClass)
+        val subFmt    = TypeApply(Select(jsonTree, "format"), subIdent :: Nil)
+        Apply(TypeApply(Ident("writeClass"), subIdent:: Nil), subName :: Ident("x") :: subFmt :: Nil)
+      }
 
       // val subFmt  = reify { Json.format[Int] }
       // val body    = Apply(Select(Literal(Constant("schoko")), "$minus$greater"), subFmt :: Nil) // reify(???).tree // EmptyTree // TODO
-
-      val body = Apply(TypeApply(Ident("writeSub"), subIdent:: Nil), Ident("x") :: Literal(Constant("schoko")) :: subFmt :: Nil)
 
       CaseDef(pat, body)
     }
     val m = Match(Ident("value"), cases)
 
-    //    val test = reify {
-    //      ("gaga": Any) match {
-    //        case x: String => x.reverse
-    //        case x: Int => x + 1
-    //      }
-    //    }
-
-    //    test match {
-    //      case Match(_, cases) =>
-    //        cases.foreach { cd =>
-    //          println("PAT")
-    //          println(cd.pat)
-    //        }
-    //    }
-
-    // println(test) // show(test))
-
     val mExpr = c.Expr[JsValue](m)
     val r     = reify {
       new Format[A] {
-        private def writeSub[A](obj: A, name: String, w: Writes[A]): JsValue =
+        private def writeClass[A1](name: String, obj: A1, w: Writes[A1]): JsValue =
           JsObject(Seq("class" -> JsString(name), "data" -> w.writes(obj)))
+
+        private def writeObject(name: String): JsValue =
+          JsObject(Seq("class" -> JsString(name)))
 
         def writes(value: A): JsValue = mExpr.splice
         def reads(json: JsValue): JsResult[A] = ???
@@ -78,15 +67,7 @@ object SealedTraitFormat {
     }
     val t = r.tree
 
-    println(t)
+    if (DEBUG) println(t)
     c.Expr[Format[A]](t)
-//    val (prod: Product, sub) = foo match {
-//      case b: Bar => (b, Json.toJson(b)(barFmt))
-//      case b: Baz => (b, Json.toJson(b)(bazFmt))
-//    }
-//    JsObject(Seq("class" -> JsString(prod.productPrefix), "data" -> sub))
   }
 }
-//trait SealedTraitFormat[A] {
-//  def writes(value: A): JsValue = macro SealedTraitFormat.writesImpl[A]
-//}
--- test/src/test/scala/play/api/libs/json/FooTest.scala
+++ test/src/test/scala/play/api/libs/json/FooTest.scala
@@ -3,6 +3,7 @@ package play.api.libs.json
 sealed trait Foo
 case class Bar(i: Int   ) extends Foo
 case class Baz(j: String) extends Foo
+case object Gaga          extends Foo
 
 object FooTest extends App {
   implicit val fooWrites = SealedTraitFormat[Foo]
@@ -14,4 +15,10 @@ object FooTest extends App {
   // implicit def fooFmt: Format[Foo] = this
   val out = Json.toJson(Bar(33))
   println(Json.prettyPrint(out))
+
+  val out1 = Json.toJson(Gaga)
+  println(Json.prettyPrint(out1))
+
+//  Json.format[Gaga.type]
+  // Json.format[Raga]
 }
