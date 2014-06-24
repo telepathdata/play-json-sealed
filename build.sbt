946edfe10401489bfa6fdd8392eccdcde842db64
Hanns Holger Rutz <contact@sciss.de>
2014-06-24 18:14:34 +0200

    v0.2.0 really. After fighting with this piece of shit called sbt for properly publishing the artifacts.

--- build.sbt
+++ build.sbt
@@ -18,51 +18,14 @@ licenses     in ThisBuild := Seq("LGPL v2.1+" -> url("http://www.gnu.org/license
 
 resolvers    in ThisBuild += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/maven-releases/"
 
-libraryDependencies in ThisBuild ++= {
-  val sv = scalaVersion.value
-  // Quasiquotes in macro paradise for Scala 2.10 require extra dependency
-  val sq0 = if (sv startsWith "2.10") {
-    val qq = "org.scalamacros" %% "quasiquotes" % "2.0.0"
-    qq :: Nil
-  } else Nil
-  //
-  sq0 ++ Seq(
-    "com.typesafe.play" %% "play-json" % "2.3.0",
-    "org.scalatest"     %% "scalatest" % "2.2.0" % "test"
-  )
-}
-
-
 // retrieveManaged in ThisBuild := true
 
 scalacOptions in ThisBuild ++= Seq("-deprecation", "-unchecked", "-feature", "-Xfuture")
-
-// ---- publishing ----
-
-publishMavenStyle in ThisBuild := true
 
 publishTo in ThisBuild :=
   Some(if (version.value endsWith "-SNAPSHOT")
     "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   else
-    "Sonatype Releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+    "Sonatype Releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
   )
-
-publishArtifact in Test := false
-
-pomIncludeRepository in ThisBuild := { _ => false }
-
-pomExtra in ThisBuild := { val n = projectName
-<scm>
-  <url>git@github.com:Sciss/{n}.git</url>
-  <connection>scm:git:git@github.com:Sciss/{n}.git</connection>
-</scm>
-<developers>
-  <developer>
-    <id>sciss</id>
-    <name>Hanns Holger Rutz</name>
-    <url>http://www.sciss.de</url>
-  </developer>
-</developers>
-}
 
--- project/Build.scala
+++ project/Build.scala
@@ -5,22 +5,74 @@ object Build extends sbt.Build {
   lazy val full: Project = Project(
     id        = "full",
     base      = file("."),
-    aggregate = Seq(core, test)
+    aggregate = Seq(core, test),
+    settings  = /* Defaults.coreDefaultSettings ++ */ Project.defaultSettings ++ Seq(
+      publish := {},
+      publishArtifact := false,
+      packagedArtifacts := Map.empty           // prevent publishing anything!
+    )
   )
+
+  private def nameCompileOnly = "compile-only"
+
+  private def projectName0 = "play-json-sealed"  // yeah, screw you too sbt
 
   lazy val core = Project(
     id        = "play-json-sealed",
     base      = file("core"),
     settings  = Project.defaultSettings ++ Seq(
+      // cf. http://stackoverflow.com/questions/21515325/add-a-compile-time-only-dependency-in-sbt
+      ivyConfigurations += config("compile-only").hide,
       // needs paradise for quasi-quotes
       resolvers += Resolver.sonatypeRepo("releases"),
-      addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
+      addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full),
+      libraryDependencies ++= {
+        val sq0 = if (scalaVersion.value startsWith "2.10")
+          ("org.scalamacros" %% "quasiquotes" % "2.0.0" % nameCompileOnly) :: Nil
+        else
+          Nil
+
+        sq0 ++ Seq(
+          "com.typesafe.play" %% "play-json" % "2.3.0"
+          // "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+        )
+      },
+      unmanagedClasspath in Compile ++= update.value.select(configurationFilter(nameCompileOnly)),
+      publishMavenStyle := true,
+      publishTo := {
+        Some(if (version.value endsWith "-SNAPSHOT")
+          "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+        else
+          "Sonatype Releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+        )
+      },
+      publishArtifact in Test := false,
+      pomIncludeRepository := { _ => false },
+      pomExtra := { val n = projectName0
+    <scm>
+      <url>git@github.com:Sciss/{n}.git</url>
+      <connection>scm:git:git@github.com:Sciss/{n}.git</connection>
+    </scm>
+      <developers>
+        <developer>
+          <id>sciss</id>
+          <name>Hanns Holger Rutz</name>
+          <url>http://www.sciss.de</url>
+        </developer>
+      </developers>
+      }
     )
   )
 
   lazy val test = Project(
     id            = "test",
     base          = file("test"),
-    dependencies  = Seq(core)
+    dependencies  = Seq(core),
+    settings      = /* Defaults.coreDefaultSettings ++ */ Project.defaultSettings ++ Seq(
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.0" % "test",
+      publish := {},
+      publishArtifact := false,
+      packagedArtifacts := Map.empty           // prevent publishing anything!
+    )
   )
 }
